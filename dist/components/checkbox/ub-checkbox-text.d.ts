8df0931c35fc7408c536411387a014cbdcbb87b4
tsunaki.okubo <14939888+ski-0n0@users.noreply.github.com>
2023-12-13 13:48:46 +0900

    dist

---
+++ dist/components/checkbox/ub-checkbox-text.d.ts
@@ -0,0 +1,24 @@
+import { LitElement } from "lit";
+export declare class UbCheckboxText extends LitElement {
+    private _checked;
+    value: string;
+    name: string;
+    set checked(val: boolean);
+    get checked(): boolean;
+    indeterminate: boolean;
+    disabled: boolean;
+    text: string | undefined;
+    input: HTMLInputElement;
+    static styles: CSSStyleSheet[];
+    protected internals: ElementInternals;
+    static formAssociated: boolean;
+    constructor();
+    private handleOnChange;
+    render(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "ub-checkbox-text": UbCheckboxText;
+    }
+}
+//# sourceMappingURL=ub-checkbox-text.d.ts.map
---
+++ dist/components/icon/ub-icon.d.ts
@@ -0,0 +1,16 @@
+import { LitElement } from "lit";
+export declare class UbIcon extends LitElement {
+    text: string;
+    type: string;
+    size?: "small" | "medium";
+    viewBox: number;
+    paths: Object;
+    static styles: CSSStyleSheet[];
+    render(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "ub-icon": UbIcon;
+    }
+}
+//# sourceMappingURL=ub-icon.d.ts.map
